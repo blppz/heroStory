0aa7d0ff1f3f13048663e150532f586f1458967c
barrylee.li <barry.ppz@gamil.com>
2019-12-12 11:15:23 +0800

    factory

--- src/main/java/org/tinygame/herostory/cmdHandler/CmdHandlerFactory.java
+++ src/main/java/org/tinygame/herostory/cmdHandler/CmdHandlerFactory.java
@@ -2,9 +2,11 @@ package org.tinygame.herostory.cmdHandler;
 
 import com.google.protobuf.GeneratedMessageV3;
 import org.tinygame.herostory.msg.GameMsgProtocol;
+import org.tinygame.herostory.util.PackageUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @Deacription TODO
@@ -16,6 +18,14 @@ public final class CmdHandlerFactory {
   }
   private static final Map<Class<?>, ICmdHandler<? extends GeneratedMessageV3>>map = new HashMap<>();
   public static void init() {
+    Set<Class<?>> clazzes = PackageUtil.listSubClazz(
+        "org.tinygame.herostory.cmdHandler",
+        true,
+        ICmdHandler.class
+    );
+    for (Class<?> clazz : clazzes) {
+      System.out.println(clazz);
+    }
     map.put(GameMsgProtocol.UserEntryCmd.class,new UserEntryCmdHandler());
     map.put(GameMsgProtocol.WhoElseIsHereCmd.class,new WhoElseIsHereCmdHandler());
     map.put(GameMsgProtocol.UserMoveToCmd.class,new UserMoveToCmdHandler());
